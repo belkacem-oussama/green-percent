25d238adfc330d8856558c9e7fd90237ff95245d
BELKACEM Oussama <belkacem-oussama@mbp-de-belkacem.home>
2023-08-25 00:26:01 +0200

    add comments and function

--- src/pages/Interview.tsx
+++ src/pages/Interview.tsx
@@ -9,24 +9,34 @@ interface Theme {
 }
 
 interface Question {
-    question: string[] | string
+    question: string[]
 }
 
 const Interview: React.FC = () => {
     const [currentTheme, setCurrentTheme] = useState<number>(0)
     const [currentQuestion, setCurrentQuestion] = useState<number>(0)
 
+    //questions
     const theme = survey.themes[currentTheme]
     let themeQuestion = theme.questions[0].question[currentQuestion]
 
     //answers
     const options = survey.themes[6].options
 
+    //functions
+    const handleInterview = () => {
+        console.log('ok')
+    }
+
     return (
         <div className="interview">
             <p>{themeQuestion}</p>
             {options.map((option, index) => (
-                <ButtonComponent key={index} text={option} />
+                <ButtonComponent
+                    key={index}
+                    text={option}
+                    onClick={handleInterview}
+                />
             ))}
         </div>
     )

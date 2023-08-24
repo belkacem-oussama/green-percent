b93040b44eb7946cf6db57554bf1f3ba7627df7a
BELKACEM Oussama <belkacem-oussama@mbp-de-belkacem.home>
2023-08-25 00:24:06 +0200

    display answers and questions

--- src/pages/Interview.tsx
+++ src/pages/Interview.tsx
@@ -1,5 +1,35 @@
-import React from 'react'
+import React, { useState } from 'react'
+import survey from '../assets/json/questions.json'
+import ButtonComponent from '../components/Button.tsx'
 
-export default function Interview() {
-    return <div>Interview</div>
+interface Theme {
+    name: string
+    questions: Question[]
+    options: string[]
 }
+
+interface Question {
+    question: string[] | string
+}
+
+const Interview: React.FC = () => {
+    const [currentTheme, setCurrentTheme] = useState<number>(0)
+    const [currentQuestion, setCurrentQuestion] = useState<number>(0)
+
+    const theme = survey.themes[currentTheme]
+    let themeQuestion = theme.questions[0].question[currentQuestion]
+
+    //answers
+    const options = survey.themes[6].options
+
+    return (
+        <div className="interview">
+            <p>{themeQuestion}</p>
+            {options.map((option, index) => (
+                <ButtonComponent key={index} text={option} />
+            ))}
+        </div>
+    )
+}
+
+export default Interview

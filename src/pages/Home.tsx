a7eb838b7facdf3d8e2d9fe84d671452eb21d347
BELKACEM Oussama <belkacem-oussama@mbp-de-belkacem.home>
2023-08-24 18:55:18 +0200

    static integration

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,5 +1,12 @@
-import React from 'react'
+import GPLogo from '../assets/images/green-percent-logo.png'
 
 export default function Home() {
-    return <div>Home</div>
+    return (
+        <div className="home-container">
+            <img src={GPLogo} />
+            <h1>Green-Percent</h1>
+            <p>à quel point es-tu vert ?</p>
+            <button>Lancer le questionnaire</button>
+        </div>
+    )
 }

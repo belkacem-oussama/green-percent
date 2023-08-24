f9144257abe8d2094edc7c2c10279d7982bf9ead
BELKACEM Oussama <belkacem-oussama@mbp-de-belkacem.home>
2023-08-24 19:39:53 +0200

    add some styles

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,15 +1,19 @@
 import GPLogo from '../assets/images/green-percent-logo.png'
 import ButtonComponent from '../assets/styles/components/Button.tsx'
-import '../assets/styles/pages/Home.scss'
+import '../assets/styles/pages/home.scss'
 
 export default function Home() {
     return (
         <div className="home">
-            <div className="home-head">
-                <img src={GPLogo} />
-                <h1>Green-Percent.</h1>
+            <div className="home-header">
+                <h1>
+                    Green-Percent<span className="green-dot">.</span>
+                </h1>
+                <img className="logo" src={GPLogo} alt="Green-Percent Logo" />
             </div>
-            <p>à quel point es-tu vert ?</p>
+            <p className="tagline">
+                À quel point es-tu <span className="green-text">vert</span> ?
+            </p>
             <ButtonComponent />
         </div>
     )

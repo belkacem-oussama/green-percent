75966f1e0154830d3c4fec24b7b51aaa2a4b3966
BELKACEM Oussama <belkacem-oussama@mbp-de-belkacem.home>
2023-08-24 18:46:16 +0200

    add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,13 @@
-const App = () => {
-  return <div>hello</div>;
-};
+import React from 'react'
+import Home from './pages/Home.tsx'
+import { Route, Routes } from 'react-router-dom'
 
-export default App;
+export default function App() {
+    return (
+        <React.Fragment>
+            <Routes>
+                <Route path="/" element={<Home />} />
+            </Routes>
+        </React.Fragment>
+    )
+}

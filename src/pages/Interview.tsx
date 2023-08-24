efc1a5ca4d59ff3d9969004142ed15847cb4388a
BELKACEM Oussama <belkacem-oussama@mbp-de-belkacem.home>
2023-08-25 01:28:52 +0200

    display current question

--- src/pages/Interview.tsx
+++ src/pages/Interview.tsx
@@ -13,41 +13,34 @@ interface Question {
 }
 
 const Interview: React.FC = () => {
-    const [currentTheme, setCurrentTheme] = useState<number>(3)
+    const [currentTheme, setCurrentTheme] = useState<number>(0)
     const [currentQuestion, setCurrentQuestion] = useState<number>(0)
 
-    //questions
+    //themes
     const theme: Theme = survey.themes[currentTheme]
     const themeName: string = theme.name
-
     const desiredTheme = survey.themes.find((theme) => theme.name === themeName)
-    const questionsByTheme = desiredTheme?.questions[0].question || []
 
-    console.log(questionsByTheme)
-
-    const question: Question = theme.questions[currentQuestion]
-
-    const themeQuestion = Array.isArray(question.question)
-        ? question.question[currentQuestion]
-        : question.question
+    //questions
+    const questionsByTheme: string[] = desiredTheme?.questions[0].question || []
+    const currentQuestionText = questionsByTheme[currentQuestion]
 
     //answers
-    const options = survey.themes[6].options
+    const options: string[] = survey.themes[6].options
 
     //functions
     const handleInterview = () => {}
 
     return (
         <div className="interview">
-            <p>{themeQuestion}</p>
-            {Array.isArray(question.question) &&
-                options.map((option, index) => (
-                    <ButtonComponent
-                        key={index}
-                        text={option}
-                        onClick={handleInterview}
-                    />
-                ))}
+            <p>{currentQuestionText}</p>
+            {options.map((option, index) => (
+                <ButtonComponent
+                    key={index}
+                    text={option}
+                    onClick={handleInterview}
+                />
+            ))}
         </div>
     )
 }
